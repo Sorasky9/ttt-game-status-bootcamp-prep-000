089f456a31592fbcae97b898cc598b9af2a0c26f
Malcom Fetus <sora.vista@gmail.com>
2020-03-15 18:24:56 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,25 @@
 # Helper Method
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
-end
 
+end
 # Define your WIN_COMBINATIONS constant
+
+ WIN_COMBINATIONS = [
+  [0,1,2], #top row
+  [3,4,5], #middle row
+  [6,7,8], #bottom row
+  [0,3,6], # left column
+  [1,4,7], # middle column
+  [2,5,8], # right column
+  [0,4,8], # diagonal from topleft column
+  [2,4,6] # diagonal from topright column
+]
+
+def won(board)
+
+ for index in WIN_COMBINATIONS
+puts index
+
+end
+end
