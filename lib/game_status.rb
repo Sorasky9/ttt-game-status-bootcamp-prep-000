4ffe6ac1347e20a8ffc43a7ea7522af2e9b200db
Malcom Fetus <sora.vista@gmail.com>
2020-03-15 19:07:41 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,6 +17,12 @@ end
 ]
 
 def won?(board)
+top_row_win = [0,1,2]
+middle_row_win = [3,4,5]
+bottom_row_win = [6,7,8]
+left_column = [0,3,6]
+middle_column_win = [1,4,7]
+right_column_win = []
 if board = WIN_COMBINATIONS
   return board
 else
