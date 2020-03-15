6dbddb636ddc1b9a4de3836e10c3d3cbb5d427f1
Malcom Fetus <sora.vista@gmail.com>
2020-03-15 22:06:16 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,7 +19,16 @@ end
 def won?(board)
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
 
-top_row_win = [0,1,2]
+win_combination = [0,1,2]
+middle_row = [3,4,5]
+bottom_row = [6,7,8]
+left_column = [0,3,6] # left column
+middle_column = [1,4,7], # middle column
+right_column = [2,5,8], # right column
+left_ diagonal = [0,4,8], # diagonal from topleft column
+top_right_column = [2,4,6] # diagonal from topright column
+
+
 
 if top_row_win
   puts top_row_win
